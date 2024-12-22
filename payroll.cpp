935df1768a0f458297c1b71166ee49382a248ac0
S-K-Hasan <syedkhairulhasan123@gmail.com>
2024-12-22 18:50:06 +0600

    khairul-2nd commit

--- payroll.cpp
+++ payroll.cpp
@@ -5,6 +5,35 @@
 #include <iomanip>
 #include <windows.h>
 using namespace std;
+class Login {
+private:
+    string LoginID,Password;
+public:
+    Login(){
+        LoginID="";
+        Password="";
+    }
+
+    void setID(string id)
+    {
+        LoginID = id;
+    }
+
+    void setPW(string pw)
+    {
+        Password = pw;
+    }
+
+    string getID()
+    {
+        return LoginID;
+    }
+
+    string getPW()
+    {
+        return Password;
+    }
+};
 
 int main(){
 
