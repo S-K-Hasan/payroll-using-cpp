49bd89627c7ce5d2968b9512d4aa7bf98b3640e1
S-K-Hasan <syedkhairulhasan123@gmail.com>
2024-12-22 19:44:11 +0600

    3rd

--- payroll.cpp
+++ payroll.cpp
@@ -34,6 +34,84 @@ public:
         return Password;
     }
 };
+void registration(Login &log)
+ {
+    system("cls");
+    string id,pw;
+    cout<<"\tEnter Login ID: ";
+    cin>>id;
+    log.setID(id);
+
+start:
+    cout<<"\tEnter A Strong Password (min 8 chars): ";
+    cin>>pw;
+    if(pw.length()>=8)
+    {
+        log.setPW(pw);
+    }
+    else
+    {
+        cout<<"\tEnter Minimum 8 Characters!" <<endl;
+        goto start;
+    }
+    ofstream outfile("Login.txt", ios::app);
+    if (!outfile)
+    {
+        cout<<"\tError: File Can't Open!"<<endl;
+    }
+    else
+    {
+        outfile<<id<<":"<<pw<<endl;
+        cout<<"\tUser Registered Successfully!"<<endl;
+    }
+    outfile.close();
+    Sleep(2000);
+}
+
+bool login()
+ {
+    system("cls");
+    string id, pw;
+    cout<<"\tEnter Login ID: ";
+    cin>>id;
+
+    cout <<"\tEnter Password: ";
+    cin>>pw;
+
+    ifstream infile("Login.txt");
+    if (!infile)
+    {
+        cout<<"\tError: File Can't Open!"<<endl;
+        return false;
+    }
+
+    string line;
+    while (getline(infile, line))
+    {
+        stringstream ss(line);
+        string userID,userPW;
+        getline(ss,userID,':');
+        getline(ss,userPW);
+
+        if (id==userID&&pw==userPW)
+        {
+            cout<<"\tLogin Successful! Welcome "<<id<<"!"<<endl;
+            infile.close();
+            Sleep(2000);
+            return true;
+        }
+    }
+
+    cout <<"\tError: Incorrect Login ID or Password!"<<endl;
+    infile.close();
+    Sleep(2000);
+    return false;
+ }
+
+int main(){
+
+
+}
 
 int main(){
 
